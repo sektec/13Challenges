e6de3069c1b681b0f72dd0729be5e3da587289d3
Gonzalo Gamez <takosaga@gmail.com>
2019-11-06 14:55:03 -0600

    Challenge 1 button

--- app/src/main/java/com/example/a13challenges/MainActivity.java
+++ app/src/main/java/com/example/a13challenges/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.a13challenges;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void updateText (View view){
+
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setText("Hello World");
     }
 }
