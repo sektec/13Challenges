11490e0e7ebb17347c27efdf516b3099c0302b36
sektec <56412044+sektec@users.noreply.github.com>
2019-11-08 14:37:54 -0600

    Edwin challenge set

--- app/src/main/java/com/example/a13challenges/MainActivity.java
+++ app/src/main/java/com/example/a13challenges/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.a13challenges;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void updateText (View view){
-
-        TextView textView = findViewById(R.id.textView);
-
-        textView.setText("Hello World");
     }
 }
